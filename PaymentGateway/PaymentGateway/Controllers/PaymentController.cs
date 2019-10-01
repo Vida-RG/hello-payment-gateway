a551da2434cd89fb8c1307e0db07ad05b9e8603c
Richárd Dévai <devai.richard@gmail.com>
2019-10-01 16:19:33 +0200

    Creation of initial API project from template

---
+++ PaymentGateway/PaymentGateway/Controllers/PaymentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentGateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
