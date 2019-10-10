4c602be428bcd5ae14336273fcd6ae6dc1647e9b
Richárd Dévai <devai.richard@gmail.com>
2019-10-10 20:07:53 +0200

    Addition of configuration settings for Cosmos and a nuget pack

---
+++ PaymentGateway/PaymentGateway.Domain/Models/CosmosDBOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace PaymentGateway.Domain.Models
+{
+    public class CosmosDBOptions
+    {
+        public string CosmosEndpoint { get; set; }
+        public string CosmosAccessKey { get; set; }
+    }
+}
